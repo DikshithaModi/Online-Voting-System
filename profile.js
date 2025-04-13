909789d703c621e2bda24266acecfa6d022b8f0f
DikshithaModi <modidikshitha@gmail.com>
2025-04-13 17:50:44 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,14 @@
+function manageElections() {
+    alert("Redirecting to Election Management Page...");
+    window.location.href = "elections.html";
+}
+
+function manageCandidates() {
+    alert("Redirecting to Candidate Management Page...");
+    window.location.href = "candidates.html";
+}
+
+function viewResults() {
+    alert("Redirecting to Results Page...");
+    window.location.href = "results.html";
+}
